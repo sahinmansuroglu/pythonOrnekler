846b79728a196fa094c5921566d27e9d0621149a
Şahin Mansuroğlu <sahinmansur@hotmail.com>
2020-12-28 11:33:21 +0300

    Create List_Kullanımı.py

---
+++ List_Kullanımı.py
@@ -0,0 +1,6 @@
+kisiselBilgi=["Görkem İnci", "Gümüşhane",15,60.5]
+
+print(f"Ad Soyad: {kisiselBilgi[0]}")
+print(f"Dogğum Yeri: {kisiselBilgi[1]}")
+print(f"Yaş: {kisiselBilgi[2]}")
+print(f"Kilo: {kisiselBilgi[3]}")
