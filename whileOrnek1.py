8eecf115192ae12b84e21c94132cb22b2034ddc6
Şahin Mansuroğlu <sahinmansur@hotmail.com>
2021-01-20 18:25:01 +0300

    Create whileOrnek1.py

---
+++ whileOrnek1.py
@@ -0,0 +1,9 @@
+# While Döngüsü şart Doğru olduğu sürece
+# çalışan döngüdür
+# eğer döngü içinde şart herhangi bir şekilde
+# false yapılmıyorsa sonsuza kadar devam eder
+
+i=0
+while(i<10):
+    i=i+1
+    print(i)
