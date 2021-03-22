0978589c0eb11a2568b65190455266427a9e296c
Şahin Mansuroğlu <sahinmansur@hotmail.com>
2021-03-22 17:58:24 +0300

    Update Quiz_23_03_21.py

--- Quiz_23_03_21.py
+++ Quiz_23_03_21.py
@@ -14,3 +14,13 @@ if girilenAd in adlar:
     print("Girilen ad listede mevcut")
 else:
     print("Girilen ad listede mevcut değil")
+
+    
+#3... bir kelimenin içerisindeki a harfi sayısını bulan program
+
+kelime="Merhabalar"
+aHarfiSayisi=0
+for herbirHarf in kelime:
+    if herbirHarf=="e":
+        aHarfiSayisi=aHarfiSayisi+1
+print(f"{aHarfiSayisi}")
