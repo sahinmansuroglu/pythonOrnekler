45ebffddbfce2eefc20122e0a474029bbe5f126b
Şahin Mansuroğlu <sahinmansur@hotmail.com>
2021-03-22 18:16:34 +0300

    Update Quiz_23_03_21.py

--- Quiz_23_03_21.py
+++ Quiz_23_03_21.py
@@ -29,3 +29,24 @@ print(f"{aHarfiSayisi}")
 print("a",end="")
 print("l",end="")
 print("i")
+
+#5. bir cümlenin tüm karakterlerinin  arasına * karakteri koyan program
+cumle="Selamar Bugün Nasılsınız?"
+
+
+for herbirHarf in cumle:
+    print(herbirHarf,end="*")
+    
+#6... Klavyeden girilen Cümlenin kelimlerini alta yazan ve kaç kelime bulunuduğunu hesaplayıp ekrana
+#yazan programı tasarlayınız
+
+cumle=input("Cümlenizi Giriniz:")
+
+kelimeSayisi=1
+for herbirHarf in cumle:
+    print(herbirHarf,end="")
+    if herbirHarf==" ":
+        kelimeSayisi=kelimeSayisi+1
+        print("")
+
+print(f"\nKelime Sayısı={kelimeSayisi}")
