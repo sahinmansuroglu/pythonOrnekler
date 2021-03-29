58caf31281f64e9d42246b5150942b56dc9dc4a7
Şahin Mansuroğlu <sahinmansur@hotmail.com>
2021-03-29 19:35:31 +0300

    Update Quiz_30_03_21.py

--- Quiz_30_03_21.py
+++ Quiz_30_03_21.py
@@ -18,3 +18,41 @@ while (True):
             i=i+1
     else:
         print("Girilen Sayi 1 ile 10 arasında değildir")
+
+#3  Çarpım tablosunu hazırlayan programı tasarlayınız
+for i in range(1,11):
+    print(f"{i} sayısının çarpım tablosu")
+    for j in range(1,11):
+        print(f"{i}x{j}={i*j}")
+#4 Bilgisayardan rastgele 10 sayı üretmesini ve bunların toplamı ile ortalamasını
+#hesaplatıp ekrana yazdırınız
+import random
+import time
+print("Size 10 tane 1 ile 1000 arasında sayı üreteceğim")
+toplam=0
+for i in range(1,11):
+    print(f"{i}. sayıyı üretiyorum..")
+    time.sleep(2)
+    rastgeleSayi=random.randint(1,1000)
+    toplam=rastgeleSayi+toplam
+    print(f"{rastgeleSayi} sayısını ürettim..")
+    print("************************")
+print("Toplam ve Ortalama hesaplatılıyor ")
+time.sleep(3)
+print(f"Urettiğim 10 sayının toplamı={toplam}")
+print(f"Urettiğim 10 sayının ortalaması={toplam/10}")
+
+#5 Bilgisayara 1 ile 10 arasında rastgele 5 adet sayı ürettiriniz:
+# Üretilen sayıların içerisinde hiçbir zaman tekrar eden sayı olmayacak
+import  random
+sayiList=[]
+i=1
+while i<=5:
+    uretilenSayi=random.randint(1,10)
+    if uretilenSayi in sayiList:
+        print(f"tekrarlı sayı:{uretilenSayi}")
+    else:
+        sayiList.append(uretilenSayi)
+        i=i+1
+
+print(sayiList)
