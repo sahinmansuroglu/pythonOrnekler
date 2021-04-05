0ed6591fe2f4432bb50542f096c47d399440bd66
Şahin Mansuroğlu <sahinmansur@hotmail.com>
2021-04-05 18:04:55 +0300

    Create Quiz_05_04_21.py

---
+++ Quiz_05_04_21.py
@@ -0,0 +1,11 @@
+#1. Ekrana 2 kez "Merhaba Dünya" yazdıran adı da ekranaMesajYaz olan
+# bir fonksiyon tanımlayın ve bu fonksiyonu 4 kez çağırın.
+def ekranaMesajYaz():
+    print("Merhaba Dünya")
+    print("Merhaba Dünya")
+
+
+ekranaMesajYaz()
+ekranaMesajYaz()
+ekranaMesajYaz()
+ekranaMesajYaz()
