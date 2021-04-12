dac0e669e534877244c5d27242d99e7402eb0e7e
Şahin Mansuroğlu <sahinmansur@hotmail.com>
2021-04-12 18:16:06 +0300

    Create Quiz_13_04_21.py

---
+++ Quiz_13_04_21.py
@@ -0,0 +1,11 @@
+#1. s1 ve s2 adında iki sayıyı paretmetre olarak alan topla adında bir fonksiyon tanımlayınız
+# bu fonksiyon içerisinde s1 ve s2 toplayacak ve sonucu döndürecek.
+# Ana programda bu fonksiyonu 2 sayıyı parametre olarak göndererek çağırınız ve dönen sonucu
+# ekrana yazdırınız.
+def topla(s1,s2):
+    sonuc=s1+s2
+    return sonuc
+
+a=topla(95,36)
+print(a)
+print("Çıkış Yapılıyor")
