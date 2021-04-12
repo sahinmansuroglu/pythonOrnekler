2f0ca8aa5d5304e29f7815f48ae1705451d5a29e
Şahin Mansuroğlu <sahinmansur@hotmail.com>
2021-04-12 19:16:51 +0300

    Update Quiz_13_04_21.py

--- Quiz_13_04_21.py
+++ Quiz_13_04_21.py
@@ -9,3 +9,67 @@ def topla(s1,s2):
 a=topla(95,36)
 print(a)
 print("Çıkış Yapılıyor")
+
+#2. Geriye değer döndüren fonksiyonlar tanımlayarak
+#Girilen 2 sayı uzerinde 4 işlem ve mod alma ve üst alma hesaplarını yaptırınız.
+# Sonucu ise geriye Değer Döndürmeyen fonksiyon ile ekrana yazdırınız.
+# fonksiyon Listesi Kullanılmadan yapılan uygulama
+
+def topla(s1,s2):
+    return  s1+s2
+def cikart(s1,s2):
+    return  s1-s2
+def carp(s1,s2):
+    return  s1*s2
+def bol(s1,s2):
+    return  s1/s2
+def modAl(s1,s2):
+    return  s1 % s2
+def usAl(s1,s2):
+    return s1**s2
+def sonucuEkranaBas(islemAdi,sonuc):
+    print(f"{islemAdi} işleminin Sonucu={sonuc}")
+def menuBasveSecenegiDondur():
+    print("İşlemler")
+    print("1-Topla")
+    print("2-Çıkart")
+    print("3-Çarp")
+    print("4-Böl")
+    print("5-Mod Al")
+    print("6-Üs Al")
+    print("0-Çıkış")
+    seciminiz=int(input("İşlem Seçiniz:"))
+    return  seciminiz
+def devamEtmekIcinEnteraBasma():
+    input("Devam Etmek için Entera basınız.....")
+def klavyedenSayiOkuVeDondur(metin):
+    return  int(input(metin))
+def secimeGoreHesaplamaYap(secim,s1,s2):
+    if secim == 1:
+        sonucuEkranaBas("Toplama",topla(s1, s2))
+    elif secim == 2:
+        sonucuEkranaBas("Çıkartma",cikart(s1, s2))
+    elif secim == 3:
+        sonucuEkranaBas("Çarpma",carp(s1, s2))
+    elif secim == 4:
+        sonucuEkranaBas("Bölme",bol(s1, s2))
+    elif secim == 5:
+        sonucuEkranaBas("Mod alma",modAl(s1, s2))
+    else:
+        sonucuEkranaBas("Üs Alma",usAl(s1, s2))
+    devamEtmekIcinEnteraBasma()
+    print("\n" * 10)
+
+while True:
+    secim=menuBasveSecenegiDondur()
+    if secim>=0 and secim<=6:
+        if secim==0:
+            print("Program Sonlandırılıyor")
+            break
+        else:
+            s1 = klavyedenSayiOkuVeDondur("1. Sayıyı Giriniz:")
+            s2 = klavyedenSayiOkuVeDondur("2. Sayıyı Giriniz:")
+            secimeGoreHesaplamaYap(secim,s1,s2)
+
+    else:
+        print("Lütfen 0 ile 6 arası değer giriniz!")
