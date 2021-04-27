e8621b3aa89ab71865bca98923352b1020715296
Şahin Mansuroğlu <sahinmansur@hotmail.com>
2021-04-27 10:57:19 +0300

    Create stringSilem5StringIcindekiBolumuAlma.py

---
+++ stringSilem5StringIcindekiBolumuAlma.py
@@ -0,0 +1,8 @@
+#Bir stringin içindeki bölümü alma
+mesaj="Bugün hava çok Güzel"
+kelime1=mesaj[6:10]# 6 ile 10. index arasını alır 6 dahil 10 dahil değil
+cumle1=mesaj[6:]#6. indexten itibaren hepsini aır
+cumle2=mesaj[:14]# 0. indexten 14. indexe kadar alır 14.index dahil değil
+print(kelime1)
+print(cumle1)
+print(cumle2)
