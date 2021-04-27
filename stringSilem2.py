e2d47cee805afea72d1b15e44483560f435cf2a2
Şahin Mansuroğlu <sahinmansur@hotmail.com>
2021-04-27 10:45:40 +0300

    Create stringSilem2.py

---
+++ stringSilem2.py
@@ -0,0 +1,8 @@
+#Stringlerde herbir karaktere erişim
+takim="Galatasaray"
+
+
+print(f"2 Karakter:{takim[1]}")
+print("Tüm harfleri alt alta yazdırıyoruz")
+for herbirHarf in takim:
+    print(herbirHarf)
